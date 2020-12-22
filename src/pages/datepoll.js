f789bca2f0e3e62bce71711068020757f1ee864c
Fabian Schedler <dev.fabian.schedler@gmail.com>
2020-12-22 12:25:35 +0100

    added datepoll page

--- src/pages/datepoll.js
+++ src/pages/datepoll.js
@@ -1,13 +1,100 @@
 import React from 'react';
+import clsx from 'clsx';
 import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+import styles from './datepoll.module.css';
+
+const features = [
+  {
+    title: 'The Idea',
+    imageUrl: 'img/idea.svg',
+    description: (
+      <>
+        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
+        ahead and move your docs into the <code>docs</code> directory.
+      </>
+    ),
+  },
+  {
+    title: 'Supported devices',
+    imageUrl: 'img/devices.svg',
+    description: (
+      <>
+        Extend or customize your website layout by reusing React. Docusaurus can
+        be extended while reusing the same header and footer.
+      </>
+    ),
+  },
+];
+
+function Feature({ imageUrl, title, description }) {
+  const imgUrl = useBaseUrl(imageUrl);
+  return (
+    <div className={clsx('col col--6', styles.feature)}>
+      {imgUrl && (
+        <div className="text--center">
+          <img className={styles.featureImage} src={imgUrl} alt={title} />
+        </div>
+      )}
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </div>
+  );
+}
 
 function Home() {
+  const context = useDocusaurusContext();
+  const { siteConfig = {} } = context;
   return (
     <Layout
       title="DatePoll"
       description="Club managing wasn't that easy before!">
+      <header className={clsx('hero hero--primary', styles.heroBanner)}>
+        <div className="container">
+          <h1 className="hero__title">DatePoll</h1>
+          <p className="hero__subtitle">Manage your Club</p>
+          <div className={styles.buttons}>
+            <Link
+              className={clsx(
+                'button button--outline button--secondary button--lg',
+                styles.getStarted,
+              )}
+              to={useBaseUrl('docs/')}>
+              Get it
+            </Link>
+          </div>
+        </div>
+      </header>
       <main>
-        <h1>DatePoll - Manage your Club</h1>
+        <div className="container">
+          <div className="row margin-vert--xl">
+            <div className={clsx("col", "col--6", styles.verticalContainer)} >
+              <div className={styles.verticalCenter}>
+                <p>
+                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+                  voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+              </p>
+              </div>
+            </div>
+            <div className="col col--6 text--center" >
+              <img src="img/team_spirit.svg" style={{maxHeight: '400px'}}></img>
+            </div>
+          </div>
+        </div>
+
+        {features && features.length > 0 && (
+          <section className={styles.features}>
+            <div className="container">
+              <div className="row">
+                {features.map((props, idx) => (
+                  <Feature key={idx} {...props} />
+                ))}
+              </div>
+            </div>
+          </section>
+        )}
       </main>
     </Layout>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,15 +3,15 @@ import Layout from '@theme/Layout';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 
 function Home() {
-    const context = useDocusaurusContext();
-    const {siteConfig = {}} = context;  
+  const context = useDocusaurusContext();
+  const { siteConfig = {} } = context;
   return (
     <Layout
       title="Home"
       description={siteConfig.tagline}>
-      <main>
-        <h1>DatePoll - Manage your Club</h1>
-      </main>
+    <main>
+      <h1 className="hero__title">DatePoll Systems</h1>
+    </main>
     </Layout>
   );
 }
