9c1957fae9703ae50794a3377ffbf875fa464de0
Fabian Schedler <dev.fabian.schedler@gmail.com>
2020-12-23 13:16:10 +0100

    changed index layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+import clsx from 'clsx';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import styles from './index.module.css';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+
 
 function Home() {
   const context = useDocusaurusContext();
@@ -9,9 +14,19 @@ function Home() {
     <Layout
       title="Home"
       description={siteConfig.tagline}>
-    <main>
-      <h1 className="hero__title">DatePoll Systems</h1>
-    </main>
+      <header className={clsx('hero hero--primary', styles.heroBanner)}>
+        <div className="container">
+          <h1 className="hero__title">{siteConfig.title}</h1>
+          <p className="hero__subtitle">{siteConfig.tagline}</p>
+          <div className={styles.buttons}>
+            <Link
+              className="button button--outline button--secondary button--lg"
+              to={useBaseUrl('projects/')}>
+              Show Projects
+            </Link>
+          </div>
+        </div>
+      </header>
     </Layout>
   );
 }
