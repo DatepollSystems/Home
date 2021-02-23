ad574d9e363fe996e43a16fb1257a39ebb399bfc
Fabian Schedler <dev.fabian.schedler@gmail.com>
2021-02-23 16:52:16 +0100

    center last projectrow

--- src/modules/Projects/ProjectList.js
+++ src/modules/Projects/ProjectList.js
@@ -2,10 +2,11 @@ import React from 'react';
 import Link from '@docusaurus/Link';
 import styles from './ProjectList.module.css';
 import projects from './Projects'
+import clsx from 'clsx';
 
 function Project({ title, shortDescription, logoUrl, link }) {
   return (
-    <div className="card text--center">
+    <div className={clsx('card text--center', styles.projectShadow)}>
       {logoUrl && (
         <div className="card__image padding-top--md">
           <img
@@ -35,14 +36,31 @@ function Project({ title, shortDescription, logoUrl, link }) {
 
 
 function ProjectList() {
+  let projectCnt = projects ? Object.keys(projects).length : 0;
+  let numRows = Math.ceil(projectCnt / 3);
+  let offsetLastRow = 0;
+  
+  switch(projectCnt % 3){
+    case 1:
+      offsetLastRow = 4;
+      break;
+    
+    case 2:
+      offsetLastRow = 2;
+      break;
+  }
+
   return (
     <>
-      {projects && Object.keys(projects).length > 0 && (
-        <div className="row">
-          {Object.keys(projects).map((name, idx) => (
-            <div className="col col--4 margin-bottom--lg">
-              <Project key={idx} {...projects[name]} />
-            </div>
+      {projectCnt > 0 && (
+        <div className={clsx('row', styles.centerLastRow)}>
+          {/* using map funtion because forEach does not return anything */}
+          {Object.entries(projects).map(([name, project], idx) => (
+            <>
+              <div key={idx} className="col col--4 margin-bottom--lg">
+                <Project key={name} {...project} />
+              </div>
+            </>
           ))}
         </div>
       )}
